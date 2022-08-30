2425cff9c7e1abf7fb16a40d78bae0720db3660f
JJongmen <pjy90123@gmail.com>
2022-08-30 18:40:47 +0900

    요구사항 3 - POST 방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,6 +14,10 @@ import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
+import util.IOUtils;
+
+import static util.HttpRequestUtils.*;
+import static util.IOUtils.*;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -29,18 +35,35 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-            String requestUri = HttpRequestUtils.parseUri(br.readLine());
+
+            // Start Line 읽기
+            String startLine = br.readLine();
+            String requestUri = parseUri(startLine);
+            String httpMethod = parseMethod(startLine);
             log.debug("Request URI = {}", requestUri);
-//            String line;
-//            while (!"".equals(line = br.readLine()) && line != null) {
-//                log.debug(line);
-//            }
-            if (requestUri.startsWith("/user/create?")) {
-                String queryString = HttpRequestUtils.getQueryString(requestUri);
-                Map<String, String> parameters = HttpRequestUtils.parseQueryString(queryString);
+            log.debug("HTTP Method = {}", httpMethod);
+
+            // HTTP Header 읽기
+            int contentLength = getContentLength(br);
+
+            // HTTP Request Body 읽기
+            String formData = null;
+            if (httpMethod.equals("POST")) {
+                formData = getFormData(br, contentLength);
+            }
+
+            if (requestUri.equals("/user/create")) {
+                Map<String, String> parameters = parseQueryString(formData);
                 User user = new User(parameters.get("userId"), parameters.get("password"), parameters.get("name"), parameters.get("email"));
                 log.debug("user = {}", user);
             }
+
+//            if (requestUri.startsWith("/user/create?")) {
+//                String queryString = HttpRequestUtils.getQueryString(requestUri);
+//                Map<String, String> parameters = HttpRequestUtils.parseQueryString(queryString);
+//                User user = new User(parameters.get("userId"), parameters.get("password"), parameters.get("name"), parameters.get("email"));
+//                log.debug("user = {}", user);
+//            }
 
             byte[] body = null;
             DataOutputStream dos = new DataOutputStream(out);
@@ -57,6 +80,26 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private int getContentLength(BufferedReader br) throws IOException {
+        String line;
+        int contentLength = 0;
+        while (!"".equals(line = br.readLine())) {
+            Pair headerPair = parseHeader(line);
+            if (headerPair.getKey().equals("Content-Length")) {
+                contentLength = Integer.parseInt(headerPair.getValue());
+                log.debug("Content-Length = {}", contentLength);
+            }
+        }
+        return contentLength;
+    }
+
+    private String getFormData(BufferedReader br, int contentLength) throws IOException {
+        String formData;
+        formData = readData(br, contentLength);
+        log.debug("formData = {}", formData);
+        return formData;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
